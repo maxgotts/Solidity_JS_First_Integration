5116480ac5f81a1a399466ad1f33f338b9ce7075
maxgotts <spamgotts@gmail.com>
2019-03-29 15:18:41 -0700

    So mf confused

--- EtherEats/contracts/OrderMeal.sol
+++ EtherEats/contracts/OrderMeal.sol
@@ -1,6 +1,14 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.5.0;
 
 contract EtherEats {
+    struct mealOrder {
+        address customerAccount;
+        address shopAccount;
+        string[] food;
+        uint[] price;
+    }
+    
+    // Helper functions
      function remove(uint index) private { /* Borrowed from Ethereum stack exchange */
         if (index >= array.length) return;
 
@@ -10,34 +18,27 @@ contract EtherEats {
         delete array[array.length-1];
         array.length--;
     }
-
-    function findElement(mealOrder[] orders, mealOrder order) view private returns (int) {
+    function findElement(mealOrder[] memory orders, mealOrder memory order) view private returns (int) {
         for (uint i=0; i < orders.length; i++) {
             if (orders[i] == order) { return i; }
         }
         return -1;
     }
 
-    event mealOrdered(mealOrder meal, timeOrdered);
-    event mealProcessed(mealOrder meal, timeProcessed);
+    // Meal events
+    //event mealOrdered(mealOrder memory meal, uint16[3] timeOrdered);
+    //event mealProcessed(mealOrder memory meal, uint16[3] timeProcessed);
 
-    struct mealOrder {
-        address customerAccount;
-        address shopAccount;
-        string[] food;
-        uint[] price;
-    }
+    mealOrder[] public pendingOrders;
+    mealOrder[] public processedOrders;
 
-    mealOrder[] public _pendingOrders;
-    mealOrder[] public _processedOrders;
-
-    function payForMeal(mealOrder meal) view public {
+    function payForMeal(mealOrder memory meal) view public {
         /* send eth to company from user account */
     }
 
-    function _moveProcessedMeal(mealOrder meal) private {
-        uint idinSlice = findElement(_pendingOrders, meal);
-       _processedOrders.push(_pendingOrders[idinSlice]);
-       remove(_pendingOrders, idinSlice);
+    function _moveProcessedMeal(mealOrder memory meal) private {
+        uint idinSlice = findElement(pendingOrders, meal);
+        processedOrders.push(pendingOrders[idinSlice]);
+        remove(pendingOrders, idinSlice);
     }
 }
