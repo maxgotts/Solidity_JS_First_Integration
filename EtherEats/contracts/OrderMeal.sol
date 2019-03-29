23093d94c8e5690bf86ec2965d80df140fba4360
maxgotts <spamgotts@gmail.com>
2019-03-29 15:01:35 -0700

    ether eats?

--- EtherEats/contracts/OrderMeal.sol
+++ EtherEats/contracts/OrderMeal.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.25;
 
 contract EtherEats {
-     function remove(uint index) private { /* Borrowed from ethereum stack exchange */
+     function remove(uint index) private { /* Borrowed from Ethereum stack exchange */
         if (index >= array.length) return;
 
         for (uint i = index; i<array.length-1; i++){
@@ -11,11 +11,11 @@ contract EtherEats {
         array.length--;
     }
 
-    function findElement(mealOrder[] orders, mealOrder order) view returns (int) {
+    function findElement(mealOrder[] orders, mealOrder order) view private returns (int) {
         for (uint i=0; i < orders.length; i++) {
-            if (orders[i] == order) { return i}
+            if (orders[i] == order) { return i; }
         }
-        return -1
+        return -1;
     }
 
     event mealOrdered(mealOrder meal, timeOrdered);
@@ -32,11 +32,11 @@ contract EtherEats {
     mealOrder[] public _processedOrders;
 
     function payForMeal(mealOrder meal) view public {
-
+        /* send eth to company from user account */
     }
 
     function _moveProcessedMeal(mealOrder meal) private {
-        uint idinSlice = findElement(_pendingOrders, meal)
+        uint idinSlice = findElement(_pendingOrders, meal);
        _processedOrders.push(_pendingOrders[idinSlice]);
        remove(_pendingOrders, idinSlice);
     }
